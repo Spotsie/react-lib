4af3a706fd040dd707c9a28dd48009a12c633883
antonioorct <antonio.orct@hotmail.com>
2022-04-11 13:44:23 +0200

    Move timeline component to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC, HTMLAttributes, ReactChild } from 'react';
+import TimelineContainer, { TimelineProps } from './Timeline/TimelineContainer';
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
   /** custom content, defaults to 'the snozzberries taste like snozzberries' */
@@ -12,4 +13,8 @@ export interface Props extends HTMLAttributes<HTMLDivElement> {
  */
 export const Thing: FC<Props> = ({ children }) => {
   return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+};
+
+export const Timeline: FC<TimelineProps> = (props) => {
+  return <TimelineContainer {...props} />;
 };
