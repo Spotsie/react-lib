9990943ccefe96892e9a65e4b03485acfa362495
antonioorct <antonio.orct@hotmail.com>
2022-04-25 09:50:11 +0200

    Remove createSelector from selectAllZonesArray

--- src/Slices/Zone/zoneSelectors.ts
+++ src/Slices/Zone/zoneSelectors.ts
@@ -1,14 +1,11 @@
-import { createSelector } from '@reduxjs/toolkit';
 import { Zone } from 'proto-all-js/deployment/organization_pb';
 import { EntityMap, entityObjectToArray } from '../utils/arrObjConversion';
 import RootState from '../utils/RootState';
 
 export const selectAllZones = (state: RootState): EntityMap<Zone.AsObject> =>
   state.zone.zones;
-export const selectAllZonesArray = createSelector(
-  selectAllZones,
-  (zones): Zone.AsObject[] => entityObjectToArray(zones)
-);
+export const selectAllZonesArray = (state: RootState) =>
+  entityObjectToArray(state.zone.zones);
 
 export const selectZonesLoading = (state: RootState): boolean =>
   state.zone.loading;
