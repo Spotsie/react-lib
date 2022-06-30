25de8237656872fab58b21ba72fface1a9a01ff9
Antonio Orct <antonio.orct@hotmail.com>
2022-06-30 21:51:34 +0200

    Change API_URL

--- src/Slices/utils/grpc.ts
+++ src/Slices/utils/grpc.ts
@@ -2,7 +2,7 @@ import { grpc } from '@improbable-eng/grpc-web';
 import { ProtobufMessage } from '@improbable-eng/grpc-web/dist/typings/message';
 import { UnaryMethodDefinition } from '@improbable-eng/grpc-web/dist/typings/service';
 
-const API_URL = 'https://api.spotsie.io';
+const API_URL = '/spotsie';
 
 type UnaryMethodResponseType<TMethod> = TMethod extends UnaryMethodDefinition<
   any,
