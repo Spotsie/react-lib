63c1ff055e1e2f9b9e9585453827573e77d14904
Antonio Orct <14859262+antonioorct@users.noreply.github.com>
2022-08-01 14:20:29 +0200

    Add timeFrame filter to selectLocationRecords

--- src/Slices/Location/locationSelectors.ts
+++ src/Slices/Location/locationSelectors.ts
@@ -2,6 +2,7 @@ import { createSelector } from '@reduxjs/toolkit';
 import { EntityMap } from '../utils/arrObjConversion';
 import { LocationHistoryRecord } from 'proto-all-js/location/location_pb';
 import RootState from '../utils/RootState';
+import { Interval } from '../utils/cache';
 
 export const selectAllLocationRecords = (
   state: RootState
@@ -9,16 +10,38 @@ export const selectAllLocationRecords = (
   state.location.locationRecords;
 
 export const selectLocationRecords = createSelector(
-  [selectAllLocationRecords, (_: any, subjectIds: number[]) => subjectIds],
-  (records, subjectIds): EntityMap<LocationHistoryRecord.AsObject[]> =>
+  [
+    selectAllLocationRecords,
+    (_: any, subjectIds: number[], timeFrame?: Interval) => ({
+      subjectIds,
+      timeFrame,
+    }),
+  ],
+  (
+    records,
+    { subjectIds, timeFrame }
+  ): EntityMap<LocationHistoryRecord.AsObject[]> =>
     Object.entries(records).reduce(
-      (obj, [subjectId, records]) =>
+      (obj, [subjectId, records]: [string, LocationHistoryRecord.AsObject[]]) =>
         subjectIds.includes(+subjectId)
-          ? { ...obj, [+subjectId]: records }
+          ? {
+              ...obj,
+              [+subjectId]: timeFrame
+                ? records.filter(
+                    (record) =>
+                      record.fromTime &&
+                      record.toTime &&
+                      record.fromTime.seconds >=
+                        timeFrame.start.getTime() / 1000 &&
+                      record.toTime.seconds <= timeFrame.end.getTime() / 1000
+                  )
+                : records,
+            }
           : obj,
       {}
     )
 );
+
 export const selectLocationRecordsLoading = (state: RootState): boolean =>
   state.location.loading;
 export const selectLocationRecordsLoaded = (state: RootState): boolean =>
