b8519d992138c440c1ed166fd52c4291215a17da
antonioorct <14859262+antonioorct@users.noreply.github.com>
2023-02-13 17:35:41 +0100

    Change color & add text

--- src/lib/Map/Layers/ApproximationLayer.tsx
+++ src/lib/Map/Layers/ApproximationLayer.tsx
@@ -14,21 +14,42 @@ const sourceProps: SourceProps & GeoJSONSourceRaw = {
   type: "geojson",
 };
 
+const trackerIdLayerProps: LayerProps = {
+  id: "tracker-id-layer",
+  type: "symbol",
+  layout: {
+    "icon-image": "marker-11",
+    "text-field": ["format", ["get", "trackerId"]],
+    "text-font": ["Open Sans Bold", "Arial Unicode MS Bold"],
+    "text-size": 14,
+    "text-transform": "uppercase",
+    "text-letter-spacing": 0.05,
+    "text-offset": [0, 1.6],
+  },
+  paint: {
+    "text-color": "#202",
+    "text-halo-color": "#fff",
+    "text-halo-width": 1,
+  },
+  filter: ["!=", "trackerId", 0],
+};
+
 const layerProps: LayerProps = {
   id: "approximation-layer",
+  beforeId: "tracker-id-layer",
   type: "circle",
   paint: {
-    "circle-color": "#e00e0e",
-    "circle-opacity": 0.3,
+    "circle-color": "#ff0000",
+    "circle-opacity": 0.4,
     "circle-radius": {
       stops: [
         [0, 0],
         [22, 200],
       ],
-      base: 2,
+      base: 1.8,
     },
-    "circle-stroke-width": 2,
-    "circle-stroke-color": "#ffef0b",
+    "circle-stroke-width": 3,
+    "circle-stroke-color": "lime",
   },
 };
 
@@ -56,6 +77,7 @@ const ApproximationLayer = ({ data, popup, onFeatureClick }: Props) => {
       {pins}
 
       <Source data={data} {...sourceProps}>
+        <Layer {...trackerIdLayerProps} />
         <Layer {...layerProps} />
       </Source>
     </>
