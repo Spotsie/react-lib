b6c1d2a91a0d5ae18c001054d6227a857bee20a7
Antonio Orct <14859262+antonioorct@users.noreply.github.com>
2023-12-21 11:06:39 +0100

    Add highlight on hover

--- src/lib/Map/Layers/ZoneLayer.tsx
+++ src/lib/Map/Layers/ZoneLayer.tsx
@@ -1,12 +1,25 @@
-import React from "react";
-import { Layer, LayerProps, Source, SourceProps } from "react-map-gl";
-import { FeatureCollection } from "../types";
+import React, { useEffect, useRef } from "react";
+import { Layer, LayerProps, Source, SourceProps, useMap } from "react-map-gl";
+import { FeatureCollection, GeoJsonProperties, Geometry } from "../types";
 
-const sourceProps: SourceProps = {
+const getSourceProps = (
+  data: FeatureCollection<Geometry, GeoJsonProperties>
+): SourceProps => ({
   id: "zone-source",
   type: "geojson",
-  cluster: false,
-};
+  generateId: true,
+  data: {
+    type: "FeatureCollection",
+    features: data.features.map((f, index) => ({
+      ...f,
+      properties: {
+        ...f.properties,
+        index,
+        geometry: (f as any).geometry,
+      },
+    })),
+  },
+});
 
 const getZoneFillLayerProps = (colors: Array<string>): LayerProps => ({
   id: "zone-fill-layer",
@@ -19,8 +32,10 @@ const getZoneFillLayerProps = (colors: Array<string>): LayerProps => ({
     } as any,
     "fill-opacity": [
       "case",
-      ["boolean", ["get", "highlighted"], false],
+      ["boolean", ["feature-state", "hover"], false],
       0.8,
+      ["boolean", ["get", "highlighted"], false],
+      0.7,
       0.2,
     ],
   },
@@ -59,28 +74,55 @@ type Props = {
   approximationMode: boolean;
 };
 
-const ZoneLayer = ({ data, colors, approximationMode }: Props) => (
-  <Source
-    data={{
-      type: "FeatureCollection",
-      features: data.features.map((f, index) => ({
-        ...f,
-        properties: {
-          ...f.properties,
-          index,
-          geometry: (f as any).geometry,
-        },
-      })),
-    }}
-    {...sourceProps}
-  >
-    <Layer {...zonePopulationLayerProps} />
-    {approximationMode ? (
-      <Layer {...getZoneFillLayerProps(colors)} beforeId="tracker-id-layer" />
-    ) : (
-      <Layer {...getZoneFillLayerProps(colors)} />
-    )}
-  </Source>
-);
+const ZoneLayer = ({ data, colors, approximationMode }: Props) => {
+  const map = useMap();
+  const hoveredPolygonId = useRef<string | number | undefined | null>(null);
+  useEffect(() => {
+    map.current?.on("mousemove", "zone-fill-layer", (e) => {
+      if (!map.current) {
+        return;
+      }
+
+      if (e.features && e.features.length > 0) {
+        if (hoveredPolygonId.current !== null) {
+          map.current.setFeatureState(
+            { source: "zone-source", id: hoveredPolygonId.current },
+            { hover: false }
+          );
+        }
+        hoveredPolygonId.current = e.features[0].id;
+
+        map.current.setFeatureState(
+          { source: "zone-source", id: hoveredPolygonId.current },
+          { hover: true }
+        );
+      }
+    });
+    map.current?.on("mouseleave", "zone-fill-layer", () => {
+      if (!map.current) {
+        return;
+      }
+
+      if (hoveredPolygonId.current !== null) {
+        map.current.setFeatureState(
+          { source: "zone-source", id: hoveredPolygonId.current },
+          { hover: false }
+        );
+      }
+      hoveredPolygonId.current = null;
+    });
+  }, []);
+
+  return (
+    <Source {...getSourceProps(data)}>
+      <Layer {...zonePopulationLayerProps} />
+      {approximationMode ? (
+        <Layer {...getZoneFillLayerProps(colors)} beforeId="tracker-id-layer" />
+      ) : (
+        <Layer {...getZoneFillLayerProps(colors)} />
+      )}
+    </Source>
+  );
+};
 
 export default ZoneLayer;
